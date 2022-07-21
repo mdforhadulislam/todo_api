f4a722d1471f9826ae819bcc0088caf8bfb93c70
FORHADUL ISLAM <mdforhadul44@gmail.com>
2022-07-21 19:05:35 +0600

    complited todo application

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+require("./dbconect");
+
+app.use(express.json());
+
+const todoRouter = require("./routes/todo");
+
+app.use("/todos/", todoRouter);
+
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `server is running on http://localhost:${process.env.PORT}/`
+  );
+});
